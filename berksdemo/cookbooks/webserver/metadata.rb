f6f3f1869e71d78c9b14a6d4928f0f8e86ed65d0
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-10-28 15:27:07 +0530

    added version changes

--- berksdemo/cookbooks/webserver/metadata.rb
+++ berksdemo/cookbooks/webserver/metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures webserver'
 long_description 'Installs/Configures webserver'
-version '0.1.0'
+version '0.1.1'
 
-#dependencies 
+#dependencies
 depends 'chef-client'
 
 
--- berksdemo/cookbooks/webserver/recipes/all_linux_nodes.rb
+++ berksdemo/cookbooks/webserver/recipes/all_linux_nodes.rb
@@ -5,3 +5,11 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 # add recipe to configure the apt-update or yum update
+
+apt-update if node["platform"] == "ubuntu" do
+    action  :update
+end
+
+yum-update if node["platform"] == "centos" do
+    action  :update
+end
