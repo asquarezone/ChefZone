0fe19cdcd20266dcd10011dc8c2c13bb6356adc0
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-12-27 12:09:33 +0530

    Added code for deploying application

--- Projects/shopizer/webserver/recipes/deploy_application.rb
+++ Projects/shopizer/webserver/recipes/deploy_application.rb
@@ -1,5 +1,17 @@
 #
-# Cookbook Name:: .
+# Cookbook Name::webserver
 # Recipe:: deploy_application
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+#deploy_application
+cookbook_file node['applicationserver']['warlocation'] do
+  source 'sm-shop.war'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+  notifies :restart, "service[#{tomcat_packageName}]"
+end
+
+log 'copied war file and restarted tomcat'
