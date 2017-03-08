d1827ab36409d4715ddddd69559849223d9a0924
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2017-03-08 18:41:34 +0530

    Added required fixes

--- Batch4/cookbooks/orangehrm/recipes/mysql_install.rb
+++ Batch4/cookbooks/orangehrm/recipes/mysql_install.rb
@@ -28,6 +28,8 @@ log 'installed mysql client'
 
 # Configure the MySQL service.
 mysql_service 'default' do
+  bind_address '0.0.0.0'
+  port '3306'
   initial_root_password 'root'
   action [:create, :start]
 end
