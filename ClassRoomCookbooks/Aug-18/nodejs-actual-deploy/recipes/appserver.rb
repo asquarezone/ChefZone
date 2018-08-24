31c0d60042f9fc15516af07cc6f05d549cdc1480
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2018-08-24 08:25:27 +0530

    idempotent execute

--- ClassRoomCookbooks/Aug-18/nodejs-actual-deploy/recipes/appserver.rb
+++ ClassRoomCookbooks/Aug-18/nodejs-actual-deploy/recipes/appserver.rb
@@ -6,15 +6,34 @@
 
 include_recipe "nodejs"
 
-include_recipe 'pm2::default'
+#include_recipe 'pm2::default'
+
+execute 'install pm2' do
+  command 'npm install -g pm2'
+  action :run
+  not_if { ::File.exist?('/home/ubuntu/installed') }
+end
 
 template '/home/ubuntu/hello.js' do
   source 'hello.js.erb'
   action :create
+  notifies :run, 'execute[cleanup]', :immedietly
 end
 
-pm2_application 'hello' do
-    script 'hello.js'
-    cwd '/home/ubuntu'
-    action [:deploy, :start_or_restart]
-  end
+execute 'start pm2' do
+  command 'pm2 start /home/ubuntu/hello.js'
+  action :run
+  not_if { ::File.exist?('/home/ubuntu/installed') }
+end
+
+file '/home/ubuntu/installed' do
+  content 'content'
+  action :create
+end
+
+
+execute 'cleanup' do
+  command 'rm /home/ubuntu/installed'
+  action :nothing
+  only_if { ::File.exist?('/home/ubuntu/installed') }
+end
