68b32399cd9887f419e650ac2acd143f6be6884d
qtdevops <qtdevops@gmail.com>
2020-09-03 08:51:25 +0530

    Added chef inspec

--- sep2020/tomcat9/test/integration/default/ubuntu_test.rb
+++ sep2020/tomcat9/test/integration/default/ubuntu_test.rb
@@ -3,14 +3,27 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-unless os.windows?
+#unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
+#  describe user('root'), :skip do
+##    it { should exist }
+#  end
+#end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+
+# This is an example test, replace it with your own test.
+describe port(8080) do
+  it { should be_listening }
 end
+
+describe file('/etc/tomcat9/tomcat-users.xml') do
+  it { should be_file }
+  
+end
+
+
+
