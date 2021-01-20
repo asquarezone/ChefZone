da19ef982413bb983c4f426f5eef1af6953ff39c
qtkhaja <qtkhaja@gmail.com>
2021-01-20 08:21:28 +0530

    Added apache installation

--- Jan21/cookbooks/qtlampserver/recipes/lamp_ubuntu.rb
+++ Jan21/cookbooks/qtlampserver/recipes/lamp_ubuntu.rb
@@ -8,3 +8,15 @@
 apt_update 'update ubuntu packages' do
     action :update
 end
+
+# sudo apt install apache2
+#apt_package 'installing apache server' do
+#    package_name 'apache2'
+#    action :install
+#end
+
+apt_package 'apache2' do
+    action :install
+end
+
+
