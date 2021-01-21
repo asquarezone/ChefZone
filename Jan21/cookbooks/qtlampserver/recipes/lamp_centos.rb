cbc049562c146e02bc39e6df8a1a12339c5ee8fc
qtkhaja <qtkhaja@gmail.com>
2021-01-21 08:07:27 +0530

    Added rest of the steps in chef resources

--- Jan21/cookbooks/qtlampserver/recipes/lamp_centos.rb
+++ Jan21/cookbooks/qtlampserver/recipes/lamp_centos.rb
@@ -4,12 +4,38 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
+# Install apache
 yum_package 'httpd' do
     action :install
 end
 
+# Start Apache
 service 'httpd' do
-    action :start
+    action :restart
 end
+
+# Install php php-mysql php-fpm
+
+yum_package 'php' do
+    action :install
+end
+
+yum_package 'php-mysql' do
+    action :install
+end
+
+yum_package 'php-fpm' do
+    action :install
+end
+
+
+file '/var/www/html/info.php' do
+    content '<?php phpinfo(); ?>'
+    action :create
+end
+
+
+
+
 
 
