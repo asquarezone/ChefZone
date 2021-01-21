2c5b08df9aafea8ae41134b0b85589f89eb84700
qtkhaja <qtkhaja@gmail.com>
2021-01-21 07:53:08 +0530

    Added resources for apache installation on  centos

--- Jan21/cookbooks/qtlampserver/recipes/lamp_centos.rb
+++ Jan21/cookbooks/qtlampserver/recipes/lamp_centos.rb
@@ -3,3 +3,13 @@
 # Recipe:: lamp_centos
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+yum_package 'httpd' do
+    action :install
+end
+
+service 'httpd' do
+    action :start
+end
+
+
