3bb4eb86be058db5b7fd7ae87eb73883cd64f772
qtkhaja <qtkhaja@gmail.com>
2021-01-21 08:16:24 +0530

    Added conditional statement & configured kitchen

--- Jan21/cookbooks/qtlampserver/recipes/default.rb
+++ Jan21/cookbooks/qtlampserver/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+if node['platform'] == 'centos'
+    include_recipe 'qtlampserver::lamp_centos'
+end
+
+if node['platform'] == 'ubuntu'
+    include_recipe 'qtlampserver::lamp_ubuntu'
+end
