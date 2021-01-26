651b2888087f13d7d0ab04e99be33cea382a2eba
qtkhaja <qtkhaja@gmail.com>
2021-01-26 08:47:34 +0530

    Added execute resources to handle non resource found or custom cases

--- Jan21/cookbooks/qttomcatserver/attributes/default.rb
+++ Jan21/cookbooks/qttomcatserver/attributes/default.rb
@@ -3,3 +3,6 @@ default['qttomcatserver']['groupname'] = 'tomcat'
 default['qttomcatserver']['username'] = 'tomcat' 
 default['qttomcatserver']['shell'] = '/bin/false'
 default['qttomcatserver']['homedir'] = '/opt/tomcat'
+default['qttomcatserver']['tomcat_download_url'] = 'https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.41/bin/apache-tomcat-9.0.41.tar.gz'
+default['qttomcatserver']['tomcat_download_location'] = "/tmp/apache-tomcat-9.0.41.tar.gz"
+default['qttomcatserver']['tomcat_location'] = '/opt/tomcat'
--- Jan21/cookbooks/qttomcatserver/recipes/ubuntu_tomcat.rb
+++ Jan21/cookbooks/qttomcatserver/recipes/ubuntu_tomcat.rb
@@ -34,5 +34,57 @@ group groupname do
     action :create
 end
 
+# variables for tomcat download location and url and home directory
+tomcat_download_url = node['qttomcatserver']['tomcat_download_url']
+tomcat_download_location = node['qttomcatserver']['tomcat_download_location']
+
+tomcat_location = node['qttomcatserver']['tomcat_location']
+
+# Create tomcat home directory
+directory tomcat_location do
+    owner user_name
+    group groupname
+    mode '775'
+    action :create
+end
+
+# download the tomcat tar file
+remote_file tomcat_download_location do
+    source tomcat_download_url
+    action :create
+    notifies :run, 'execute[untartomcat]', :immediate
+end
+
+# untar the tomcat tar to /opt/tomcat
+# lets execute manual linux commands 
+# note manual linux commands are not idemponent
+execute 'untartomcat' do
+    command "sudo tar xzvf #{tomcat_download_location} -C #{tomcat_location} --strip-components=1"
+    action :nothing
+end
+
+execute 'give tomcat ownership' do
+    command 'sudo chgrp -R tomcat /opt/tomcat'
+    action :nothing
+    subscribes :run, 'execute[untartomcat]', :immediate
+end
+
+execute 'give permission to conf' do
+    command 'sudo chmod -R g+r /opt/tomcat/conf && sudo chmod g+x /opt/tomcat/conf'
+    action :nothing
+    subscribes :run, 'execute[untartomcat]', :immediate
+end
+
+execute 'recursive give ownership' do
+    command 'sudo chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/'
+    action :nothing
+    subscribes :run, 'execute[untartomcat]', :immediate
+end
+
+
+
+
+
+
 
 
