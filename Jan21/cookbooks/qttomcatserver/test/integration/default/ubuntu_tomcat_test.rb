4e94c6fee48f390a11111921796e8915bce4f55b
qtkhaja <qtkhaja@gmail.com>
2021-01-28 08:03:40 +0530

    Added tests

--- Jan21/cookbooks/qttomcatserver/test/integration/default/ubuntu_tomcat_test.rb
+++ Jan21/cookbooks/qttomcatserver/test/integration/default/ubuntu_tomcat_test.rb
@@ -3,14 +3,33 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('openjdk-8-jdk') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe group('tomcat') do
+  it { should exist }
+end
+
+describe user('tomcat') do
+  it { should exist }
+  its('group') { should eq 'tomcat' }
+end
+
+describe file('/opt/tomcat') do
+  it { should exist }
+end
+
+
+describe file('/tmp/apache-tomcat-9.0.41.tar.gz') do
+  it { should exist }
+end
+
+describe service('tomcat') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(8080) do
+  it { should be_listening }
 end
