f9d928798b8ea7d51286e115656696a59d1a64ce
qtkhaja <qtkhaja@gmail.com>
2021-01-29 08:29:59 +0530

    Added mongo db dependency

--- Jan21/cookbooks/supermarketdemo/recipes/default.rb
+++ Jan21/cookbooks/supermarketdemo/recipes/default.rb
@@ -5,3 +5,4 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 #include_recipe 'supermarketdemo::mysqlconfig'
 include_recipe 'supermarketdemo::configuretomcat'
+include_recipe "sc-mongodb::default"
