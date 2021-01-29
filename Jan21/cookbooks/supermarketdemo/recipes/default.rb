3d27ae8ebf5a317e2970fd43166b3877242d7f74
qtkhaja <qtkhaja@gmail.com>
2021-01-29 08:37:40 +0530

    Added nginx dependency

--- Jan21/cookbooks/supermarketdemo/recipes/default.rb
+++ Jan21/cookbooks/supermarketdemo/recipes/default.rb
@@ -5,4 +5,8 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 #include_recipe 'supermarketdemo::mysqlconfig'
 include_recipe 'supermarketdemo::configuretomcat'
-include_recipe "sc-mongodb::default"
+#include_recipe "sc-mongodb::default"
+
+nginx_install 'nginx' do
+    source 'distro'
+end
