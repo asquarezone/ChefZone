76a8bda7623d282aa99527a5c3ce2e87b616bc1c
qtpython <qtkhajacloud@gmail.com>
2021-11-02 08:44:46 +0530

    tried to use array

--- Oct21/chef-starter/chef-repo/cookbooks/apache_phpinfo/recipes/default.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/apache_phpinfo/recipes/default.rb
@@ -5,34 +5,20 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
 apt_update 'update ubuntu packages' do
-    ignore_failure true
-    action :update
+  ignore_failure true
+  action :update
 end
 
-package 'apache2' do
-    action :install
+package 'install lamp packages' do
+  package_name ['apache2', 'php', 'libapache2-mod-php', 'php-mysql']
+  action :install
 end
-
-package 'php' do
-    action :install
-end
-
-package 'libapache2-mod-php' do
-    action :install
-end
-
-package 'php-mysql' do
-    action :install
-end
-
 
 file '/var/www/html/info.php' do
-    content '<?php phpinfo(); ?>'
-    action :create
+  content '<?php phpinfo(); ?>'
+  action :create
 end
 
 service 'apache2' do
-    action :restart
+  action :restart
 end
-
-
