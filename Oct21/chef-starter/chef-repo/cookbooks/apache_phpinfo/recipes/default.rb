f4fede4d07510e18c5227a2d82a1156166995ddc
qtpython <qtkhajacloud@gmail.com>
2021-11-04 08:20:25 +0530

    Added changes

--- Oct21/chef-starter/chef-repo/cookbooks/apache_phpinfo/recipes/default.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/apache_phpinfo/recipes/default.rb
@@ -3,22 +3,23 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+if node['platform'] == 'ubuntu'
+  apt_update 'update ubuntu packages' do
+    ignore_failure true
+    action :update
+  end
 
-apt_update 'update ubuntu packages' do
-  ignore_failure true
-  action :update
-end
+  package 'install lamp packages' do
+    package_name %w(apache2 php libapache2-mod-php php-mysql)
+    action :install
+  end
 
-package 'install lamp packages' do
-  package_name %w(apache2 php libapache2-mod-php php-mysql)
-  action :install
-end
+  file '/var/www/html/info.php' do
+    content '<?php phpinfo(); ?>'
+    action :create
+  end
 
-file '/var/www/html/info.php' do
-  content '<?php phpinfo(); ?>'
-  action :create
-end
-
-service 'apache2' do
-  action :restart
+  service 'apache2' do
+    action :restart
+  end
 end
