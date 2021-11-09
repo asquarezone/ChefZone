77baa9eafd87e70624c4cd33f010eb554007848a
qtpython <qtkhajacloud@gmail.com>
2021-11-09 08:45:47 +0530

    value for platform

--- Oct21/chef-starter/chef-repo/cookbooks/apache_phpinfo/attributes/default.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/apache_phpinfo/attributes/default.rb
@@ -1,9 +1,22 @@
 # default attributes
-default['apache_phpinfo']['apache_package'] = 'apache2'
-default['apache_phpinfo']['all_packages'] = %w(apache2 php libapache2-mod-php php-mysql)
+default['apache_phpinfo']['apache_package'] = value_for_platform(
+  ['centos', 'redhat', 'suse', 'fedora' ] => {
+    'default' => 'httpd'
+    },
+    ['ubuntu', 'debian'] => {
+      'default' => 'apache2'
+    }
+  )
+
+default['apache_phpinfo']['all_packages'] = value_for_platform(
+    ['centos', 'redhat', 'suse', 'fedora' ] => {
+      'default' => %w(httpd php php-mysql php-fpm)
+      },
+      ['ubuntu', 'debian'] => {
+        'default' => %w(apache2 php libapache2-mod-php php-mysql)
+      }
+    )
+
 default['apache_phpinfo']['info_page_path'] = '/var/www/html/info.php'
 
-if platform?('centos')
-  default['apache_phpinfo']['apache_package'] = 'httpd'
-  default['apache_phpinfo']['all_packages'] = %w(httpd php php-mysql php-fpm)
-end
+
