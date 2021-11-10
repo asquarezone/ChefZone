b98ad57041d8329e37cec51839effcd632086e5c
qtpython <qtkhajacloud@gmail.com>
2021-11-10 09:03:31 +0530

    Corrected the group and default.rb

--- Oct21/chef-starter/chef-repo/cookbooks/tomcat9/recipes/configure.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/tomcat9/recipes/configure.rb
@@ -7,7 +7,6 @@
 # sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
 user node['tomcat9']['username'] do
   comment 'comment'
-  gid node['tomcat9']['groupname']
   home node['tomcat9']['userhome']
   shell node['tomcat9']['usershell']
   action :create
--- Oct21/chef-starter/chef-repo/cookbooks/tomcat9/recipes/default.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/tomcat9/recipes/default.rb
@@ -5,3 +5,4 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
 include_recipe 'tomcat9::java'
+include_recipe 'tomcat9::configure'
