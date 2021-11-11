849657b2fd965bb3d715eac319f7d688e0c643ff
qtpython <qtkhajacloud@gmail.com>
2021-11-11 08:17:22 +0530

    Download and extract the tomcat

--- Oct21/chef-starter/chef-repo/cookbooks/tomcat9/attributes/default.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/tomcat9/attributes/default.rb
@@ -6,3 +6,6 @@ default['tomcat9']['username'] = 'tomcat'
 default['tomcat9']['groupname'] = 'tomcat'
 default['tomcat9']['userhome'] = '/opt/tomcat'
 default['tomcat9']['usershell'] = '/bin/false'
+default['tomcat9']['version'] = '9.0.54'
+default['tomcat9']['download_url'] = "https://archive.apache.org/dist/tomcat/tomcat-9/v#{node['tomcat9']['version']}/bin/apache-tomcat-#{node['tomcat9']['version']}.tar.gz"
+default['tomcat9']['local_download_path'] = "/tmp/apache-tomcat-#{node['tomcat9']['version']}.tar.gz"
--- Oct21/chef-starter/chef-repo/cookbooks/tomcat9/recipes/configure.rb
+++ Oct21/chef-starter/chef-repo/cookbooks/tomcat9/recipes/configure.rb
@@ -5,9 +5,25 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
 # sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+user_home = node['tomcat9']['userhome']
+
 user node['tomcat9']['username'] do
   comment 'comment'
-  home node['tomcat9']['userhome']
+  home user_home
   shell node['tomcat9']['usershell']
   action :create
+end
+
+download_url = node['tomcat9']['download_url']
+local_download_path = node['tomcat9']['local_download_path']
+
+remote_file local_download_path do
+  source download_url
+  action :create
+end
+
+archive_file 'tomcat' do
+  path local_download_path
+  destination user_home
+  action :extract
 end
