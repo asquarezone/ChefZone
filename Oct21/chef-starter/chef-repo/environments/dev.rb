be59aac4e883a6af56bf9c308295fc7e0ef2eb19
qtdevops <qtdevops@gmail.com>
2021-11-20 20:11:25 +0530

    Added environments

---
+++ Oct21/chef-starter/chef-repo/environments/dev.rb
@@ -0,0 +1,6 @@
+name 'dev'
+description 'developer environment'
+cookbook_versions(
+    'experiment' => '>= 1.0.0',
+    'tomcat9' => '>= 1.0.0'
+)
---
+++ Oct21/chef-starter/chef-repo/environments/prod.rb
@@ -0,0 +1,6 @@
+name 'prod'
+description 'production environment'
+cookbook_versions(
+    'experiment' => '= 1.0.0',
+    'tomcat9' => '= 1.0.0'
+)
