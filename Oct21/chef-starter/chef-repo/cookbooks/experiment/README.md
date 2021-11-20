b8006e4069b9fb4e74eeed2b2332eb3e36c81308
qtdevops <qtdevops@gmail.com>
2021-11-20 19:13:38 +0530

    Added a new cookbook with simple log resource

---
+++ Oct21/chef-starter/chef-repo/cookbooks/experiment/README.md
@@ -0,0 +1,4 @@
+# experiment
+
+TODO: Enter the cookbook description here.
+
---
+++ Oct21/chef-starter/chef-repo/cookbooks/experiment/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: experiment
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+log 'This is version 1.0.0' do
+    level :info
+end
