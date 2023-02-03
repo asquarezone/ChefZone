116d840549563168e08e52d7830dd32debc7036d
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-02-03 16:25:50 +0530

    Added variables documentation

--- Scripting/Feb23/Basics/backup.sh
+++ Scripting/Feb23/Basics/backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# get date in the ddmmYYYY format and store it in a variable called as today
 today=$(date +"%d%m%Y")
+#create a variable called as backup_dir
 backup_dir="/opt/backup/qtdevops-${today}"
+#create a variable called as source_directory
 source_directory="/home/qtdevops"
 mkdir -p $backup_dir
 echo "Copying files from ${source_directory}"
---
+++ Scripting/Feb23/Basics/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 1
+echo 2
+echo 3
+echo 4
+echo 5
