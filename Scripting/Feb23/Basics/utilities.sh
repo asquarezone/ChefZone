6bec5eb7738fd9ef612dd0f9a888554332aad524
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-02-07 17:18:09 +0530

    Added functions

---
+++ Scripting/Feb23/Basics/utilities.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# function definition to get distribution
+get_distribution() {
+    distribution=""
+    # Check if /etc/os-release  file exists
+    if [ -r /etc/os-release ]; then
+        distribution="$(. /etc/os-release && echo "$ID")"
+    fi
+    echo $distribution
+
+}
+
+# usage
+echo $(get_distribution)
